239de6c674e901b29ad89f7e4281ec898c490de5
PerecerDev <perecer.dev@gmail.com>
2023-10-24 07:16:59 +0200

    fixed size for price bigimagetext

--- components/big-text-image/big-text-image.php
+++ components/big-text-image/big-text-image.php
@@ -10,7 +10,7 @@ $img = RTEMU . '/assets/img/componentes/pensando2.png';
     <aside class="w-full relative z-20 p-12 !pt-32 text-right text-teal-900 bg-orange-200">
         
         <h2 class="w-full font-serif text-6xl font-bold mb-10 text-white">¿Cuánto cuesta una web?</h2>
-        <h3 class="w-full font-sans text-4xl font-thin text-right text-stone-500 mb-20 pl-32">Desde 890 €</h3>
+        <h3 class="w-full font-sans text-4xl font-thin text-right text-stone-500 mb-20 pl-36">Desde 890 €</h3>
 
     </aside>
     <aside class="w-full overflow-hidden absolute z-20 -bottom-48 ">
