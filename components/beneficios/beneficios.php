f2c1eedb9612fac4d998b29ceae7a2a52effe94b
PerecerDev <perecer.dev@gmail.com>
2023-10-29 12:49:54 +0100

    fixing buttons contact

--- components/beneficios/beneficios.php
+++ components/beneficios/beneficios.php
@@ -9,22 +9,22 @@ defined('ABSPATH') OR die;
         <ul class="grid md:grid-cols-2 gap-x-8 gap-y-8 text-green-300 mb-8">
             <li class="list-none rounded-md bg-green-950/50 text-center font-sans ring-1 ring-green-300 py-7 px-8 mb-8">
                 <div class="text-7xl font-bold font-sans mb-5">97<span class="text-xl"> %</span></div>
-                <span class="text-white text-lg w-full block font-thin">Buscan negocios locales antes de salir a comprar</span>
+                <span class="text-white text-lg w-full block font-light">Buscan negocios locales antes de salir a comprar</span>
                 <a href="https://www.google.es" class="text-xs font-thin">google.es</a>
             </li>
             <li class="list-none rounded-md bg-green-950/50 text-center font-sans ring-1 ring-green-300 py-7 px-8 mb-8">
                 <div class="text-7xl font-bold font-sans mb-5">84<span class="text-xl"> %</span></div>
-                <span class="text-white text-lg w-full block font-thin">Consideran más creíble y fiable un negocio con web</span>
+                <span class="text-white text-lg w-full block font-light">Consideran más creíble y fiable un negocio con web</span>
                 <a href="https://www.verisign.es" class="text-xs font-thin">verisign.es</a>
             </li>
             <li class="list-none rounded-md bg-green-950/50 text-center font-sans ring-1 ring-green-300 py-7 px-8 mb-8">
                 <div class="text-7xl font-bold font-sans mb-5">29<span class="text-xl"> %</span></div>
-                <span class="text-white text-lg w-full block font-thin">No harían negocios con empresas sin web</span>
+                <span class="text-white text-lg w-full block font-light">No harían negocios con empresas sin web</span>
                 <a href="https://www.clutch.es" class="text-xs font-thin">clutch.es</a>
             </li>
             <li class="list-none rounded-md bg-green-950/50 text-center font-sans ring-1 ring-green-300 py-7 px-8 mb-8">
                 <div class="text-7xl font-bold font-sans mb-5">3<span class="text-xl"> X</span></div>
-                <span class="text-white text-lg w-full block font-thin">Clientes potenciales a partir de una web</span>
+                <span class="text-white text-lg w-full block font-light">Clientes potenciales a partir de una web</span>
                 <a href="https://www.hubspot.es" class="text-xs font-thin">hubspot.es</a>
             </li>
         </ul>
