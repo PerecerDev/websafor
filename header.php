30a734796513b3533ce9238a91f55a06fe88baa9
PerecerDev <perecer.dev@gmail.com>
2023-11-01 20:51:01 +0100

    seguimiento conversiones email

--- header.php
+++ header.php
@@ -12,6 +12,8 @@ defined('ABSPATH') OR die;
 		<meta name="description" content="Astro description" />
 		<title>WebSafor - Tu propia web</title>
 		<?php wp_head(); ?>
+
+		
 		<!-- Google tag (gtag.js) -->
 		<script async src="https://www.googletagmanager.com/gtag/js?id=G-GPRNVPPKWH"></script>
 		<script>
@@ -21,6 +23,21 @@ defined('ABSPATH') OR die;
 
 		gtag('config', 'G-GPRNVPPKWH');
 		</script>
+		<!-- Google tag (gtag.js) -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=AW-11385693493"></script>
+		<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', 'AW-11385693493');
+		</script>
+		<!-- Event snippet for Contacto conversion page -->
+		<script>
+		gtag('event', 'conversion', {'send_to': 'AW-11385693493/RGYeCIvlq-8YELXKj7Uq'});
+		</script>
+
+
 		
 		<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
 		<script defer src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" integrity="sha384-rOA1PnstxnOBLzCLMcre8ybwbTmemjzdNlILg8O7z1lUkLXozs4DHonlDtnE7fpc" crossorigin="anonymous"></script>
