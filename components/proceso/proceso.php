fc4a41837a71175cf1d3e567ff6199284ca81473
PerecerDev <perecer.dev@gmail.com>
2023-11-01 06:56:02 +0100

    proceso simplificado

--- components/proceso/proceso.php
+++ components/proceso/proceso.php
@@ -14,8 +14,8 @@ defined('ABSPATH') OR die;
             
             <div class="flex flex-col items-center">
                 <div class="flex-shrink-0 w-16 h-16 rounded-full mb-4 inline-flex items-center text-center justify-center bg-teal-900 text-white relative z-10 title-font font-medium text-2xl">1</div>
-                <h2 class="font-medium title-font text-teal-900 mb-1 text-xl">Acuerdo de Web</h2>
-                <p class="leading-relaxed text-center">Descripción detallada del acuerdo de web, incluyendo los términos y condiciones, expectativas y otros detalles relevantes.</p>
+                <h2 class="font-medium title-font text-teal-900 mb-1 text-xl">Ideación de Web</h2>
+                <p class="leading-relaxed text-center">Con la información que nos prporciones, idearemos el proyecto para que se adapte a las necesidades de tu negocio.</p>
             </div>
         </div>
         <!-- Paso 2: Firma de contrato -->
@@ -24,50 +24,28 @@ defined('ABSPATH') OR die;
             <div class="flex flex-col items-center">
                 <div class="flex-shrink-0 w-16 h-16 rounded-full mb-4 inline-flex items-center text-center justify-center bg-teal-900 text-white relative z-10 title-font font-medium text-2xl">2</div>
                 <h2 class="font-medium title-font text-teal-900 mb-1 text-xl">Firma de contrato</h2>
-                <p class="leading-relaxed text-center">Descripción detallada de la firma del contrato, incluyendo los términos específicos, fechas clave y otros aspectos importantes del acuerdo.</p>
+                <p class="leading-relaxed text-center">Para comenzar a desarrollar la web, es necesario firmar un contrato con las características acordadas y un abono del 50% del total presupuestado.</p>
             </div>
         </div>
-        <!-- Paso 3: Depósito del 50% -->
-        <div class="flex relative pt-10 pb-20 sm:items-center md:w-2/3 mx-auto text-center">
-            
-            <div class="flex flex-col items-center">
-                <div class="flex-shrink-0 w-16 h-16 rounded-full mb-4 inline-flex items-center text-center justify-center bg-teal-900 text-white relative z-10 title-font font-medium text-2xl">3</div>
-                <h2 class="font-medium title-font text-teal-900 mb-1 text-xl">Depósito del 50%</h2>
-                <p class="leading-relaxed text-center">Descripción detallada del proceso de depósito. Esto incluirá cómo se debe realizar el pago, las fechas límite y cualquier otro detalle relevante.</p>
-            </div>
-        </div>
-
+        
         <!-- Paso 4: Desarrollo del proyecto -->
         <div class="flex relative pt-10 pb-20 sm:items-center md:w-2/3 mx-auto text-center">
             
             <div class="flex flex-col items-center">
-                <div class="flex-shrink-0 w-16 h-16 rounded-full mb-4 inline-flex items-center text-center justify-center bg-teal-900 text-white relative z-10 title-font font-medium text-2xl">4</div>
+                <div class="flex-shrink-0 w-16 h-16 rounded-full mb-4 inline-flex items-center text-center justify-center bg-teal-900 text-white relative z-10 title-font font-medium text-2xl">3</div>
                 <h2 class="font-medium title-font text-teal-900 mb-1 text-xl">Desarrollo del proyecto</h2>
-                <p class="leading-relaxed text-center">Descripción detallada del proceso de desarrollo. Esto incluirá las etapas clave, las revisiones, las comunicaciones con el cliente y cualquier otro aspecto importante del desarrollo.</p>
+                <p class="leading-relaxed text-center">Una vez firmado el contrato y abonado el depósito, comenzaremos con el desarrollo de la web, terminándola en el plazo acordado.</p>
             </div>
         </div>
         <!-- Paso 5: Restante 50% -->
         <div class="flex relative pt-10 pb-20 sm:items-center md:w-2/3 mx-auto text-center">
             
             <div class="flex flex-col items-center">
-                <div class="flex-shrink-0 w-16 h-16 rounded-full mb-4 inline-flex items-center text-center justify-center bg-teal-900 text-white relative z-10 title-font font-medium text-2xl">5</div>
-                <h2 class="font-medium title-font text-teal-900 mb-1 text-xl">Restante 50%</h2>
-                <p class="leading-relaxed text-center">Descripción detallada del proceso de pago del saldo restante. Esto incluirá cómo se debe realizar el pago, las fechas límite y cualquier otro detalle relevante.</p>
-            </div>
-        </div>
-
-        <!-- Paso 6: Entrega de proyecto -->
-        <div class="flex relative pt-10 pb-20 sm:items-center md:w-2/3 mx-auto text-center">
-            
-            <div class="flex flex-col items-center">
-                <div class="flex-shrink-0 w-16 h-16 rounded-full mb-4 inline-flex items-center text-center justify-center bg-teal-900 text-white relative z-10 title-font font-medium text-2xl">6</div>
+                <div class="flex-shrink-0 w-16 h-16 rounded-full mb-4 inline-flex items-center text-center justify-center bg-teal-900 text-white relative z-10 title-font font-medium text-2xl">4</div>
                 <h2 class="font-medium title-font text-teal-900 mb-1 text-xl">Entrega de proyecto</h2>
-                <p class="leading-relaxed text-center">Descripción detallada del proceso de entrega del proyecto. Esto incluirá cómo se entregará el proyecto, las revisiones finales, la formación del cliente y cualquier otro aspecto importante de la entrega.</p>
+                <p class="leading-relaxed text-center">Una vez la web se ha terminado de desarrollar, se abonará el restante 50% del total acordado y se entregará la web completada.</p>
             </div>
         </div>
-
-
-
 
 
     <!-- Paso 2: Firma de contrato -->
