9f45b70b669a794f6c3f77e09362f12e2ea79fd6
PerecerDev <perecer.dev@gmail.com>
2023-11-05 09:39:47 +0100

    out favicon code header

--- header.php
+++ header.php
@@ -38,18 +38,7 @@ defined('ABSPATH') OR die;
 		<a title="Web Analytics" href="https://clicky.com/101432110" style="display:none;"><img alt="Clicky" src="//static.getclicky.com/media/links/badge.gif" border="0" /></a>
 		<script async data-id="101432110" src="//static.getclicky.com/js"></script>
 
-
-
-		
-		<link rel="icon" type="image/svg+xml" href="/favicon.svg" /> 
 		<script defer src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" integrity="sha384-rOA1PnstxnOBLzCLMcre8ybwbTmemjzdNlILg8O7z1lUkLXozs4DHonlDtnE7fpc" crossorigin="anonymous"></script>
-		<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-		<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-		<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-		<link rel="manifest" href="/site.webmanifest">
-		<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
-		<meta name="msapplication-TileColor" content="#0c4847">
-		<meta name="theme-color" content="#0c4847">
 	</head>
 
 	<body style="{ margin:0; padding:0;}">
